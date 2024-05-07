3ee1833c11a6b2aad9132ea63fb786a955fd6124
vinothmp21102005 <mpvinoth2180@gmail.com>
2024-05-07 14:28:38 +0530

    commit

--- fifo1.c
+++ fifo1.c
@@ -1,1 +1,11 @@
 //C Program that illustrate communication between two process using named pipes
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+int main(){
+int res = mkfifo("/tmp/my_fifo", 0777);
+if (res == 0) printf("FIFO created\n");
+exit(EXIT_SUCCESS);
+}
